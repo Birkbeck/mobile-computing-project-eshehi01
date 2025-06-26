17958ee586aaed3e7a1261f529077eeebdd46adf
Emmy888-png <emmyshemmy888@gmail.com>
2025-06-26 20:05:06 +0100

    Created MainFragment.kt to represent the home screen using a fragment. Also added fragment_main.xml as the associated layout file (currently empty).

---
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/MainFragment.kt
@@ -0,0 +1,16 @@
+package co.uk.bbk.culinarycompanion_enriketashehi
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity).setTitle("Main")
+    }
+}
