8a0f4974c287765e39f93964a875a04f9eed3370
Emmy888-png <emmyshemmy888@gmail.com>
2025-06-26 19:25:51 +0100

    COURSEWORK 2 Commence adding functionality to app - first step is implementing Navigation Drawer - add navigation component dependencies to build.gradle.kts - this ensures the necessary navigation libraries are available.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,20 @@ android {
 }
 
 dependencies {
-
+    // Default dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Navigation component dependencies
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.7")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.7")
+
+    // Default dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
