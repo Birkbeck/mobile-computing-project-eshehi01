1889c0df129f991e778c9391cb61ec1fbe284708
Emmy888-png <emmyshemmy888@gmail.com>
2025-06-26 21:02:43 +0100

    Resolved unresolved references in MainFragment.kt by adding necessary imports for Fragment and FragmentMainBinding. This allows the fragment to compile successfully and prepares it for use in the navigation system.

--- app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/MainFragment.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/MainFragment.kt
@@ -1,5 +1,12 @@
 package co.uk.bbk.culinarycompanion_enriketashehi
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import co.uk.bbk.culinarycompanion_enriketashehi.databinding.FragmentMainBinding
+
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
