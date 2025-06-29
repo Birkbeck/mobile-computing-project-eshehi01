9bb638d5fee651faa253ce8766c5f12f8ad741f0
Emmy888-png <emmyshemmy888@gmail.com>
2025-06-29 19:24:27 +0100

    Created new fragments for recipe detail and create/edit screens. Updated navigation graph with new fragments and adjusted labels for consistency.

---
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeDetailFragment.kt
@@ -0,0 +1,28 @@
+package co.uk.bbk.culinarycompanion_enriketashehi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import co.uk.bbk.culinarycompanion_enriketashehi.databinding.FragmentRecipeDetailBinding
+
+class RecipeDetailFragment : Fragment() {
+
+    private var _binding: FragmentRecipeDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecipeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
