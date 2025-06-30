c4ab2441ca50f8e6e9baf551482c2173bf7788eb
Emmy888-png <emmyshemmy888@gmail.com>
2025-06-30 21:38:58 +0100

    Updated data model in Recipe.kt to include all required Recipe fields.

--- app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/Recipe.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/Recipe.kt
@@ -2,5 +2,8 @@ package co.uk.bbk.culinarycompanion_enriketashehi
 
 data class Recipe (
     val title: String,
-    val preview: String
+    val ingredients: String,
+    val instructions: String,
+    val category: String,
+    val preview: String // A short description of the recipe for displaying in list
 )
