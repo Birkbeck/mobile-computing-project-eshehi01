0768538a5a8119669b4ce665f4da9c44dd756465
Emmy888-png <emmyshemmy888@gmail.com>
2025-06-30 16:20:47 +0100

    Created RecipeAdapter.kt to feed data into the recycler view in fragment_main.xml. Wrapped code in recipe_card_item.xml in <layout> root tag so that it now implements DataBinding and generates the binding class RecipeCardItemBinding as required by RecipeAdapter.kt.

---
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeAdapter.kt
@@ -0,0 +1,37 @@
+package co.uk.bbk.culinarycompanion_enriketashehi
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.uk.bbk.culinarycompanion_enriketashehi.databinding.RecipeCardItemBinding
+
+class RecipeAdapter(
+    private val recipes: List<Recipe>,
+    private val onItemClick: (Recipe) -> Unit
+) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+
+    inner class RecipeViewHolder(private val binding: RecipeCardItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(recipe: Recipe) {
+            binding.recipeTitle.text = recipe.title
+            binding.recipePreview.text = recipe.preview
+
+            binding.viewDetailsButton.setOnClickListener {
+                onItemClick(recipe)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = RecipeCardItemBinding.inflate(inflater, parent, false)
+        return RecipeViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = recipes.size
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.bind(recipes[position])
+    }
+}
