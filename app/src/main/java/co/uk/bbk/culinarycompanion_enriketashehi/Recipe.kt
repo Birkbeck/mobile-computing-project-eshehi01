7329f32cd3ddd3fa07e414ea843ef1c3384da7ba
Emmy888-png <emmyshemmy888@gmail.com>
2025-07-02 11:59:29 +0100

    Updated Recipe.kt so that the recipe table is represented by an entity as required by Room. Also defined a primary key for the recipes.

--- app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/Recipe.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/Recipe.kt
@@ -1,8 +1,15 @@
 package co.uk.bbk.culinarycompanion_enriketashehi
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName= "recipe")
 data class Recipe (
+    // Unique ID for each recipe, auto-generated
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
-    val preview: String, // A short description of the recipe for displaying in list
+    // A short description of the recipe for displaying in list
+    val preview: String,
     val ingredients: String,
     val instructions: String,
     val category: String
