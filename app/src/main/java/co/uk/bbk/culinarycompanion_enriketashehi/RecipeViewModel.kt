5a159e301f31c057d67bedc517833180b2721ccc
Emmy888-png <emmyshemmy888@gmail.com>
2025-07-04 13:43:14 +0100

    Updated code in RecipeViewModel.kt to interact with RecipeRepository, rather than directly with RecipeDAO. Modified insertRecipe and getAllRecipes functions accordingly. Added updateRecipe and deleteRecipe functions.

--- app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeViewModel.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeViewModel.kt
@@ -5,21 +5,46 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
+// ViewModel for Recipe data, connects UI with data layer
 class RecipeViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val recipeDao = RecipeDatabase
-        .getDatabase(application)
-        .recipeDao()
+    private val repository: RecipeRepository
 
+    init {
+        val dao = RecipeDatabase
+            .getDatabase(application)
+            .recipeDao()
+        repository = RecipeRepository(dao)
+    }
+
+    // Insert new recipe asynchronously
     fun insertRecipe(recipe: Recipe) {
         viewModelScope.launch {
-            recipeDao.insertRecipe(recipe)
+            repository.insertRecipe(recipe)
         }
     }
 
+    // Get all recipes - suspending function for use with coroutines
     suspend fun getAllRecipes(): List<Recipe> {
-        return recipeDao.getAllRecipes()
+        return repository.getAllRecipes()
     }
 
-    // Additional functions to be added
+    // Get recipe by ID - suspending function
+    suspend fun getRecipeById(id: Int): Recipe? {
+        return repository.getRecipeById(id)
+    }
+
+    // Update recipe asynchronously
+    fun updateRecipe(recipe: Recipe) {
+        viewModelScope.launch {
+            repository.updateRecipe(recipe)
+        }
+    }
+
+    // Delete recipe asynchronously
+    fun deleteRecipe(recipe: Recipe) {
+        viewModelScope.launch {
+            repository.deleteRecipe(recipe)
+        }
+    }
 }
