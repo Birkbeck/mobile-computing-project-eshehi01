6f68912695ef9060defe934de50cbf82d854cf93
Emmy888-png <emmyshemmy888@gmail.com>
2025-07-04 13:33:23 +0100

    Added and modified code in RecipeDetailFragment.kt to implement ViewModel logic and to load recipe data from the ViewModel. Also implemented passing of recipeId to Create/Edit Recipe screen when a recipe is selected for editing. Also added code to display recipe details on the Recipe Detail screen.

--- app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeDetailFragment.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeDetailFragment.kt
@@ -5,16 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import co.uk.bbk.culinarycompanion_enriketashehi.databinding.FragmentRecipeDetailBinding
+import kotlinx.coroutines.launch
 
 class RecipeDetailFragment : Fragment() {
+
+    private val viewModel: RecipeViewModel by viewModels()
 
     private var _binding: FragmentRecipeDetailBinding? = null
     private val binding get() = _binding!!
 
     private val args: RecipeDetailFragmentArgs by navArgs()
+
+    private var currentRecipeId: Int = -1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,12 +35,23 @@ class RecipeDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        currentRecipeId = args.recipeId
+
         // Update the activity title
         requireActivity().title = getString(R.string.recipe_detail)
 
-        // Set the text fields using arguments passed from MainFragment
-        binding.recipeTitleTextView.text = args.recipeTitle
-        binding.ingredientsTextView.text = args.recipePreview
+        // Load the recipe data from the ViewModel/database
+        if (currentRecipeId != -1) {
+            viewLifecycleOwner.lifecycleScope.launch {
+                val recipe = viewModel.getRecipeById(currentRecipeId)
+                if (recipe != null) {
+                    displayRecipe(recipe)
+                } else {
+                    // Handle recipe not found
+                    binding.recipeTitleTextView.text = getString(R.string.recipe_not_found)
+                }
+            }
+        }
 
         // Handle back button click
         binding.backButton.setOnClickListener {
@@ -42,8 +60,17 @@ class RecipeDetailFragment : Fragment() {
 
         // Handle Edit Recipe button click
         binding.editRecipeButton.setOnClickListener {
-            findNavController().navigate(R.id.action_recipeDetailFragment_to_createEditRecipeFragment)
+            // Pass recipeId to Create/Edit screen for editing
+            val action = RecipeDetailFragmentDirections
+                .actionRecipeDetailFragmentToCreateEditRecipeFragment(recipeId = currentRecipeId)
+            findNavController().navigate(action)
         }
+    }
+
+    private fun displayRecipe(recipe: Recipe) {
+        binding.recipeTitleTextView.text = recipe.title
+        binding.ingredientsTextView.text = recipe.ingredients
+        binding.instructionsTextView.text = recipe.instructions
     }
 
     override fun onDestroyView() {
