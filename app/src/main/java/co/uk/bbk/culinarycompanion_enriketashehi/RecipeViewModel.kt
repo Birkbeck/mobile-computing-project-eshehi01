26c6e2912337fcd2093e6e5778d66e3b26cd88ad
Emmy888-png <emmyshemmy888@gmail.com>
2025-07-05 12:28:46 +0100

    Refactored RecipeViewModel.kt to expose LiveData and remove suspend recipe loading from UI layer.

--- app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeViewModel.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion_enriketashehi/RecipeViewModel.kt
@@ -2,6 +2,7 @@ package co.uk.bbk.culinarycompanion_enriketashehi
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
@@ -10,27 +11,27 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
 
     private val repository: RecipeRepository
 
+    val allRecipes: LiveData<List<Recipe>>
+
     init {
         val dao = RecipeDatabase
             .getDatabase(application)
             .recipeDao()
         repository = RecipeRepository(dao)
+
+        // Initialize LiveData for all recipes
+        allRecipes = repository.getAllRecipes()
     }
 
-    // Insert new recipe asynchronously
+    // Insert new recipe
     fun insertRecipe(recipe: Recipe) {
         viewModelScope.launch {
             repository.insertRecipe(recipe)
         }
     }
 
-    // Get all recipes - suspending function for use with coroutines
-    suspend fun getAllRecipes(): List<Recipe> {
-        return repository.getAllRecipes()
-    }
-
-    // Get recipe by ID - suspending function
-    suspend fun getRecipeById(id: Int): Recipe? {
+    // Get recipe by ID
+    fun getRecipeById(id: Int): LiveData<Recipe?> {
         return repository.getRecipeById(id)
     }
 
